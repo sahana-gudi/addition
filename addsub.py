c32b056930a071d8e18ae1aec35f8e98ecacd0c6
sahana-gudi <sonugudi472@gmail.com>
2025-10-15 10:43:50 +0530

    Create addsub.py

---
+++ addsub.py
@@ -0,0 +1,8 @@
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+
+sum_result = a + b
+sub_result = a - b
+print("Sum:", sum_result)
+print("Subtraction:", sub_result)
+ 
